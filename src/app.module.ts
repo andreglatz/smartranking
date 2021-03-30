12e2000f308087496ac4b29262243da7047e3f84
André Glatz <andre.luiz@expressotaubate.com.br>
2021-03-30 18:47:51 -0300

    add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:JZJTpMhm8JqAFd54@cluster0.0zbf6.mongodb.net/smartranking',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
+    PlayersModule,
+  ],
   controllers: [],
   providers: [],
 })
