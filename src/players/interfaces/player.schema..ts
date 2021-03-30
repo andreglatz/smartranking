1ffe4b0614a3b350e4f9e291d59202252a40bba2
André Glatz <andre.luiz@expressotaubate.com.br>
2021-03-30 20:23:31 -0300

    remove unique constraint from phoneNumber

--- src/players/interfaces/player.schema..ts
+++ src/players/interfaces/player.schema..ts
@@ -2,8 +2,8 @@ import { Schema } from 'mongoose';
 
 export const PlayerSchema = new Schema(
   {
-    phoneNumber: { type: String, unique: true },
     email: { type: String, unique: true },
+    phoneNumber: String,
     name: String,
     ranking: String,
     rankingPosition: Number,
