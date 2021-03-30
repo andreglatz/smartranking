2e615c169bab97c3a9bd07d5728f397b6d2f5f95
André Glatz <andre.luiz@expressotaubate.com.br>
2021-03-30 19:26:59 -0300

    add database repository to player service

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,28 +1,30 @@
-import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { SavePlayerDto } from './dtos/save-player.dto';
 import { Player } from './interfaces/player.interface';
-import { v4 } from 'uuid';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class PlayersService {
-  #logger = new Logger(PlayersService.name);
-  #players: Player[] = [];
+  constructor(
+    @InjectModel('player') private readonly playerModule: Model<Player>,
+  ) {}
 
   async savePlayer(savePlayerDto: SavePlayerDto): Promise<void> {
     const { email } = savePlayerDto;
 
-    const playerFound = this.#players.find((player) => player.email == email);
+    const playerFound = await this.playerModule.findOne({ email }).exec();
 
     if (playerFound) this.update(playerFound, savePlayerDto);
     else this.save(savePlayerDto);
   }
 
   async loadAll(): Promise<Player[]> {
-    return this.#players;
+    return await this.playerModule.find().exec();
   }
 
   async findByEmail(email: string): Promise<Player> {
-    const playerFound = this.#players.find((player) => player.email == email);
+    const playerFound = await this.playerModule.findOne({ email }).exec();
 
     if (!playerFound)
       throw new NotFoundException(`Player with email ${email} not found`);
@@ -30,37 +32,21 @@ export class PlayersService {
     return playerFound;
   }
 
-  async delete(email: string): Promise<void> {
-    const playerFound = this.#players.find((player) => player.email === email);
-
-    if (!playerFound)
-      throw new NotFoundException(`Player with email ${email} not found`);
-
-    this.#players = this.#players.filter(
-      (player) => player.email !== playerFound.email,
-    );
+  async delete(email: string): Promise<any> {
+    return await this.playerModule.deleteOne({ email }).exec();
   }
 
-  private update(playerFound: Player, savePlayerDto: SavePlayerDto): void {
-    const { name } = savePlayerDto;
-    playerFound.name = name;
+  private async update(
+    player: Player,
+    savePlayerDto: SavePlayerDto,
+  ): Promise<Player> {
+    return await this.playerModule
+      .findOneAndUpdate({ email: player.email }, { $set: savePlayerDto })
+      .exec();
   }
 
-  private save(savePlayerDto: SavePlayerDto): void {
-    const { name, email, phoneNumber } = savePlayerDto;
-
-    const player: Player = {
-      _id: v4(),
-      name,
-      email,
-      phoneNumber,
-      ranking: 'A',
-      positionRanking: 1,
-      urlPhoto: 'https://google.com/foto123.jpg',
-    };
-
-    this.#logger.log(`save player: ${JSON.stringify(player)}`);
-
-    this.#players.push(player);
+  private async save(savePlayerDto: SavePlayerDto): Promise<Player> {
+    const player = new this.playerModule(savePlayerDto);
+    return await player.save();
   }
 }
