a78d5371b9c59476748f4be371955920152de5c0
André Glatz <andre.luiz@expressotaubate.com.br>
2021-03-29 22:47:08 -0300

    add save player route

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { SavePlayerDto } from './dtos/save-player.dto';
+import { PlayersService } from './players.service';
 
 @Controller('api/v1/players')
 export class PlayersController {
+  constructor(private readonly playersService: PlayersService) {}
+
   @Post()
-  async savePlayer() {
-    return JSON.stringify({ name: 'André' });
+  async savePlayer(@Body() savePlayerDto: SavePlayerDto) {
+    await this.playersService.savePlayer(savePlayerDto);
   }
 }
---
+++ src/players/players.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { SavePlayerDto } from './dtos/save-player.dto';
+import { Player } from './interfaces/players.interface';
+import { v4 } from 'uuid';
+
+@Injectable()
+export class PlayersService {
+  #logger = new Logger(PlayersService.name);
+  #players: Player[] = [];
+
+  async savePlayer(savePlayerDto: SavePlayerDto): Promise<void> {
+    this.save(savePlayerDto);
+  }
+
+  private save(savePlayerDto: SavePlayerDto): void {
+    const { name, email, phoneNumber } = savePlayerDto;
+
+    const player: Player = {
+      _id: v4(),
+      name,
+      email,
+      phoneNumber,
+      ranking: 'A',
+      positionRanking: 1,
+      urlPhoto: 'https://google.com/foto123.jpg',
+    };
+
+    this.#logger.log(`save player: ${JSON.stringify(player)}`);
+
+    this.#players.push(player);
+  }
+}
