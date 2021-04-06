e60979985307cdbde7ad29c35000f01076eec643
André Glatz <andreglatz@outlook.com>
2021-04-06 20:12:53 -0300

    add http exception filter

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,39 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  private readonly logger = new Logger(AllExceptionsFilter.name);
+
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const statusCode =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException ? exception.getResponse() : '';
+
+    this.logger.error(
+      `Http Status Code: ${statusCode} | Error Message: ${JSON.stringify(
+        message,
+      )}`,
+    );
+
+    response.status(statusCode).json({
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error: message,
+    });
+  }
+}
